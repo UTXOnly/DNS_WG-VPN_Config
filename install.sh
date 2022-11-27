fab7fa47cd5bbc0de78ddd417c72ec575c4ea203
Brian Hartford <bhartford419@gmail.com>
2022-11-26 22:06:45 -0500

    Add dd install script into main

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-
+BGreen='\u001b[32;1m'
+NC='\033[0m'
 echo "Do you want to install Datadog agent?"
 bash dd_install.sh
 wait
-
+echo -e "${BGreen}Datadog agent installed successfully, moving on...${NC}"
 git clone https://github.com/UTXOnly/sudo_user_create.git
 cd sudo_user_create
 sudo bash init.sh
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 public_ip_address="$(curl -Ls ifconfig.me)"
+echo -e "${BGreen}\nYour publis ip is: ${public_ip_address}${NC}"
 sudo apt install ufw -y
 sudo allow 22/tcp
 sudo allow from $public_ip_address to any port 53 proto udp
 sudo ufw enable
+echo -e "${BGreen}\nFirewall active, installing pihole"
 curl -sSL https://install.pi-hole.net | bash
 
