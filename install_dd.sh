3651efd6fcd192070fc22933ad225c638870da52
Brian Hartford <bhartford419@gmail.com>
2022-11-26 21:23:45 -0500

    Add dd status

--- install_dd.sh
+++ install_dd.sh
@@ -8,3 +8,4 @@ DD_API_KEY=${API_KEY} DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amaz
 sudo cp ./datadog.yaml /etc/datadog-agent/
 echo -e "\napi_key: ${API_KEY}" | sudo tee -a /etc/datadog-agent/datadog.yaml
 sudo service datadog-agent restart
+sudo datadog-agent status
