79ac60bb1b4d0b6141191324f89184fde1ce4b3c
Brian Hartford <bhartford419@gmail.com>
2022-11-27 12:08:05 -0500

    Added ufw rule for port 80 for pihole admin interface

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ echo -e "${BGreen}\nYour public ip is: ${public_ip_address}"
 sudo apt install ufw -y
 sudo ufw allow 22/tcp
 sudo ufw allow from $public_ip_address to any port 53 proto udp
+sudo ufw allow from $public_ip_address to any port 80
 sudo ufw enable
 echo -e "\nUFW Firewall active, installing pihole${NC}"
 #unattended-upgrades.service 
