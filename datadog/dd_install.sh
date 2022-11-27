d7f44b255197e7b693b9cc69bcd2394d87517070
Brian Hartford <bhartford419@gmail.com>
2022-11-27 00:00:06 -0500

    adjusted color codes on prompt

--- datadog/dd_install.sh
+++ datadog/dd_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo "Please enter your DD_API_KEY and press ENTER: "
+BGreen='\u001b[32;1m'
+NC='\033[0m'
+echo -e "${BGreen}Please enter your DD_API_KEY and press ENTER:${NC} "
 read API_KEY
 
 DD_API_KEY=${API_KEY} DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
