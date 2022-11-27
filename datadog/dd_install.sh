3fcf13f00aa5d4c9bb3922ee30015103710bb053
Brian Hartford <bhartford419@gmail.com>
2022-11-26 23:21:57 -0500

    Added /datadog

---
+++ datadog/dd_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter your DD_API_KEY and press ENTER: "
+read API_KEY
+
+DD_API_KEY=${API_KEY} DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
+
+sudo cp ./datadog/datadog.yaml /etc/datadog-agent/
+echo -e "\napi_key: ${API_KEY}" | sudo tee -a /etc/datadog-agent/datadog.yaml
+sudo service datadog-agent restart
+sleep 7
+sudo datadog-agent status
