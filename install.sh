9ad43d89d7f64b2d0944b5ecb5abcabad04ef4b7
Brian Hartford <bhartford419@gmail.com>
2022-11-26 20:20:05 -0500

    Add restart

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/UTXOnly/sudo_user_create.git
 cd sudo_user_create
 sudo bash init.sh
 
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y
 public_ip_address="$(curl -Ls ifconfig.me)"
 sudo apt install ufw -y
 sudo allow 22/tcp
