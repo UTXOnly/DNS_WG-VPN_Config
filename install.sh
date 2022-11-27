46aec9af15439059d625e296bca4e3a11f2aaaf7
Brian Hartford <bhartford419@gmail.com>
2022-11-26 19:00:23 -0500

    Add install dd, pihole, create user

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+git clone https://github.com/UTXOnly/sudo_user_create.git
+cd sudo_user_create
+sudo bash init.sh
+
 sudo apt-get update && sudo apt-get upgrade
 public_ip_address="$(curl -Ls ifconfig.me)"
 sudo apt install ufw -y
 sudo allow 22/tcp
 sudo allow from $public_ip_address to any port 53 proto udp
 sudo ufw enable
+curl -sSL https://install.pi-hole.net | bash
 
