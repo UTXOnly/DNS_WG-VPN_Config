ebd01c293a13c4c9d33d693d359e2ad367fad7bb
Brian Hartford <bhartford419@gmail.com>
2022-11-27 01:29:57 -0500

    changed some color codes

--- install.sh
+++ install.sh
@@ -17,12 +17,12 @@ sudo bash init.sh
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 public_ip_address="$(curl -Ls ifconfig.me)"
-echo -e "${BGreen}\nYour public ip is: ${public_ip_address}${NC}"
+echo -e "${BGreen}\nYour public ip is: ${public_ip_address}"
 sudo apt install ufw -y
 sudo ufw allow 22/tcp
 sudo ufw allow from $public_ip_address to any port 53 proto udp
 sudo ufw enable
-echo -e "${BGreen}\nUFW Firewall active, installing pihole${NC}"
+echo -e "\nUFW Firewall active, installing pihole${NC}"
 #unattended-upgrades.service 
 curl -sSL https://install.pi-hole.net | bash
 
