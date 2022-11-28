b57f1d36a4709185d65a71ebd7488f727b9ec4e5
Brian Hartford <bhartford419@gmail.com>
2022-11-27 23:33:51 -0500

    Add quotes around CONFIGS variable

--- datadog/dd_install.sh
+++ datadog/dd_install.sh
@@ -9,12 +9,9 @@ echo -e "${NC}"
 DD_API_KEY="$API_KEY" DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
 wait
 
-
 CONFIGS=$(< ./datadog/dd_configs)
-echo $CONFIGS | sudo tee -a /etc/datadog-agent/datadog.yaml 
+echo "$CONFIGS" | sudo tee -a /etc/datadog-agent/datadog.yaml 
 
-#echo -e "\napi_key: ${API_KEY}" | sudo tee -a /etc/datadog-agent/datadog.yaml
-#echo $CONFIGS | sudo tee -a /etc/datadog-agent/datadog.yaml
 sudo service datadog-agent restart
 sleep 7
 sudo datadog-agent status
