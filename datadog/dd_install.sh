f3aaae2338c6de37fe6f3eff19910ff9eef635de
Brian Hartford <bhartford419@gmail.com>
2022-11-27 20:49:54 -0500

    ?

--- datadog/dd_install.sh
+++ datadog/dd_install.sh
@@ -6,7 +6,7 @@ echo -e "${BGreen}"
 read -p "Please enter your DD_API_KEY and press ENTER: " -s API_KEY
 #read API_KEY
 echo"${NC}"
-DD_API_KEY="" DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
+DD_API_KEY="$API_KEY" DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
 
 sudo cp ./datadog/datadog.yaml /etc/datadog-agent/
 
