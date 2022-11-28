f8075386e0b33dff503a3760695360d9ce7b4e6e
Brian Hartford <bhartford419@gmail.com>
2022-11-27 20:36:02 -0500

    update api key method

--- datadog/dd_install.sh
+++ datadog/dd_install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 BGreen='\u001b[32;1m'
 NC='\033[0m'
-echo -e "${BGreen}Please enter your DD_API_KEY and press ENTER:${NC} "
-read API_KEY
-
-DD_API_KEY=${API_KEY} DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
+#echo -e "${BGreen}"
+#echo -e "${BGreen}Please enter your DD_API_KEY and press ENTER:"
+#read API_KEY
+DD_API_KEY=$(< .env)
+DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
 
 sudo cp ./datadog/datadog.yaml /etc/datadog-agent/
-echo -e "\napi_key: ${API_KEY}" | sudo tee -a /etc/datadog-agent/datadog.yaml
+
+#echo -e "\napi_key: ${API_KEY}" | sudo tee -a /etc/datadog-agent/datadog.yaml
+echo $DD_API_KEY >> /etc/datadog-agent/datadog.yaml
 sudo service datadog-agent restart
 sleep 7
 sudo datadog-agent status
