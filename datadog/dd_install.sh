fd0131666b196d5ac0f3c18860053136cf48f48e
Brian Hartford <bhartford419@gmail.com>
2022-12-18 16:38:56 -0500

    Add NPM config

--- datadog/dd_install.sh
+++ datadog/dd_install.sh
@@ -12,6 +12,13 @@ wait
 CONFIGS=$(< ./datadog/dd_configs)
 echo "$CONFIGS" | sudo tee -a /etc/datadog-agent/datadog.yaml 
 
+sudo -u dd-agent install -m 0644 /etc/datadog-agent/system-probe.yaml.example /etc/datadog-agent/system-probe.yaml
+
+sudo tee -a >/etc/datadog-agent/system-probe.yaml << EOF
+network_config:
+  enabled: true
+EOF
+
 sudo service datadog-agent restart
 sleep 7
 sudo datadog-agent status
